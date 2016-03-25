349f0279178daa8d9830f2927503354ffd4cfb99
Lyubomir <poseidon4o.random@gmail.com>
2016-03-25 17:16:15 +0200

    Add teamcity artifacts

--- build/builder/linux.py
+++ build/builder/linux.py
@@ -90,27 +90,7 @@ class LinuxBuilder(Builder):
 
 
 	def package(self):
-		pass
-		# subdir = "linux" + "/" + self.build_arch
+		releasePath = os.path.join(self.dir_release, 'V-Ray', 'VRayZmqServer', 'VRayZmqServer')
 
-		# release_path = os.path.join(self.dir_release, subdir)
-
-		# if not self.mode_test:
-		# 	utils.path_create(release_path)
-
-		# archive_name = utils.GetPackageName(self)
-		# archive_path = utils.path_join(release_path, archive_name)
-
-		# sys.stdout.write("Generating archive: %s\n" % (archive_name))
-		# sys.stdout.write("  in: %s\n" % (release_path))
-
-		# cmd = "tar jcf %s %s" % (archive_path, self.dir_install_name)
-
-		# sys.stdout.write("Calling: %s\n" % (cmd))
-		# sys.stdout.write("  in: %s\n" % (self.dir_install))
-
-		# if not self.mode_test:
-		# 	os.chdir(self.dir_install)
-		# 	os.system(cmd)
-
-		# return subdir, archive_path
+		sys.stdout.write("##teamcity[setParameter name='env.ENV_ARTEFACT_FILES' value='%s']" % releasePath)
+		sys.stdout.flush()
