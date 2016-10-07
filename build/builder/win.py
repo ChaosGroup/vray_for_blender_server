e196a18445d1b2d117a5d44d0277f46ca1e4c4d7
Lyubomir Koev <lyubomir.koev@chaosgroup.com>
2016-10-07 19:32:20 +0300

    Fix LIBS_ROOT for different build dir

--- build/builder/win.py
+++ build/builder/win.py
@@ -50,7 +50,7 @@ class WindowsBuilder(Builder):
 		cmake.append('-DAPPSDK_PATH=%s' % os.environ['CGR_APPSDK_PATH'])
 		cmake.append('-DAPPSDK_VERSION=%s' % os.environ['CGR_APPSDK_VERSION'])
 
-		cmake.append('-DLIBS_ROOT=%s' % utils.path_join(self.dir_build, '..', 'blender-for-vray-libs'))
+		cmake.append('-DLIBS_ROOT=%s' % utils.path_join(self.dir_source, '..', 'blender-for-vray-libs'))
 
 		cmake.append("../vrayserverzmq")
 
